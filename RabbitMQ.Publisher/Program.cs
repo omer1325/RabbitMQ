ebafd1e13eaef37e7ea7cf3e34c0739ef1d58a30
Ömer Faruk İngeç <omer.ingec24@gmail.com>
2022-01-15 21:05:46 +0300

    Work Queue

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kqzmktud:ESpgbAqKzq_uSrdLQp2tbc1O1oFe4AM3@toad.rmq.cloudamqp.com/kqzmktud");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+             {
+                 string message = $"Message {x}";
+
+                 //to byte
+                 var messageBody = Encoding.UTF8.GetBytes(message);
+
+                 channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                 Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+             });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subcriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Subcriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kqzmktud:ESpgbAqKzq_uSrdLQp2tbc1O1oFe4AM3@toad.rmq.cloudamqp.com/kqzmktud");
+            using var connection = factory.CreateConnection() ;
+
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+            // channel.QueueDeclare("hello-queue", true, false, false);
+            
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("hello-queue", false, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
